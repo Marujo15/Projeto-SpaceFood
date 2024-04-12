af1c8635e2e7cc9e09c7eab857702cbb30158866
Kaliane Marques <kalianemarques@outlook.com>
2024-04-12 04:02:09 -0300

    upadates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const config = require('./config');
 const express = require('express');
+const path = require('path');
 const app = express();
+const routes = require('./routes');
 const port = config.PORT;
 const cookieParser = require('cookie-parser');
 
@@ -8,21 +10,27 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-const routes = require('./routes');
-app.use('/api', routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', function (req, res, next) {
+app.get('/login', (req, res) => {
   res.clearCookie('session_id');
-  next();
-}, routes);
+  res.sendFile(path.join(__dirname, 'public/login/index.html'));
+});
 
-app.use('/home', function (req, res, next) {
-  if (req.cookies.session_id) {
-    next();
-  } else {
-    res.redirect('/');
-  }
-}, routes);
+app.get('/register', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/register/index.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/home/index.html'));
+});
+
+// Rota padrão para a página inicial
+app.get('/', function(req, res) {
+  res.redirect("/login");
+});
+
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://45.63.13.91:${port}`);
--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const config = {
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT || 3000,
     SECRET_KEY: process.env.SECRET_KEY || "chaveQualquer",
-    DB_HOST: process.env.DB_HOST || "192.168.011",
+    DB_HOST: process.env.DB_HOST || "192.168.0.11",
     DB_USER: process.env.DB_USER || "postgres",
     DB_NAME: process.env.DB_NAME || "spacefood",
     DB_PASSWORD: process.env.DB_PASSWORD || "jiejie",
