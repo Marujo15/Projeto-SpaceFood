0fa40cac3d29340708e2e7c0774ce584cedcfd9d
Kaliane Marques <kalianemarques@outlook.com>
2024-04-21 20:35:55 -0300

    atualização

--- src/config/index.js
+++ src/config/index.js
@@ -5,10 +5,10 @@ const config = {
     PORT: process.env.PORT || 3000,
     ADDRESS: process.env.ADDRESS || 'localhost',
     SECRET_KEY: process.env.SECRET_KEY || "chaveQualquer",
-    DB_HOST: process.env.DB_HOST || "192.168.0.11",
+    DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER || "postgres",
     DB_NAME: process.env.DB_NAME || "spacefood",
-    DB_PASSWORD: process.env.DB_PASSWORD || "teste",
+    DB_PASSWORD: process.env.DB_PASSWORD || "jiejie",
     SESSION_DURATION: process.env.SESSION_DURATION || "1h",
 }
 
