410108cf4d8de611635f760a24fe9c2ad0222f2e
Kaliane Marques <kalianemarques@outlook.com>
2024-04-23 19:12:38 -0300

    Correção na ordem de execução do código

--- src/app.js
+++ src/app.js
@@ -6,21 +6,21 @@ const routes = require('./routes');
 const port = config.PORT;
 const ip = config.ADDRESS;
 const cookieParser = require('cookie-parser');
-const loginPage = require('./public/login');
-const registerPage = require('./public/register');
-const homePage = require('./public/home');
+
+const publicPath = path.join(__dirname, 'public')
 
 app.use(express.json());
-app.use('/static', express.static(path.resolve(__dirname, 'public', 'static')))
+app.use(express.static(publicPath))
 app.use('/assets', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
 
-app.get('/', loginPage)
-app.get('/login', loginPage)
-app.get('/register', registerPage)
-app.get('/home', homePage)
-
 app.use('/api', routes);
+
+app.get('/*', (req, res)=>{
+  res.sendFile(path.resolve('src','public', 'index.html'));//aqui
+})
+
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://${ip}:${port}`);
