09c129798d063122a32f461034d54e147f411851
Denner Willian <dennerwillian01@gmail.com>
2024-05-02 19:10:52 +0000

    Adjusted app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const config = require('./src/config');
 const express = require('express');
-const path = require('path');
 const app = express();
 const routes = require('./src/routes');
 const port = config.PORT;
@@ -9,13 +8,8 @@ const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.json({ limit: '50mb' }));
 
 app.use('/api', routes);
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('public', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://${ip}:${port}`);
